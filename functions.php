18f91b9a6852b1cd8f669f63475fb2377a91affb
Hugo Trippaers <htrippaers@schubergphilis.com>
2013-10-16 10:15:06 +0200

    When multiple speakers are present, don't show just the first one

--- functions.php
+++ functions.php
@@ -151,7 +151,7 @@ function schedule($day)
 				break;
 			}
 			
-			if(isset($info->speakers[0]->name))
+			if(count($info->speakers) == 1 && isset($info->speakers[0]->name))
 			{
 				$titlePlus = '<div class="more-info-click">+</div>';
 				$speaker = '<span class="speaker"><h2 class="speaker">By <a href="'.$info->speakers[0]->web_url.'" target="_blank" title="'.$info->speakers[0]->name.'" alt="'.$info->speakers[0]->name.'" >'.$info->speakers[0]->name.'</a></h2></span>';
@@ -171,6 +171,10 @@ function schedule($day)
 							</tr>
 						</table>
 					</div>';
+			}
+			else 
+			{
+				$speaker = '<span class="speaker"><h2 class="speaker">Multiple Speakers</h2></span>'
 			}
 			
 			$table .= '<tr id="'.$info->event_id.'" data-url="'.$info->web_url.'" class="'.$color.'">
